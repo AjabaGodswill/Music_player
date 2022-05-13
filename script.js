5b3a94a98902766f1e403c7e818ae8e7cfa242a0
AjabaGodwill <ajabagodswill@gmail.com>
2022-05-13 22:21:44 +0100

    Tried using arrow functions and template lterals

--- script.js
+++ script.js
@@ -24,6 +24,12 @@ let updateTimer;
 
 const music_list = [
     {
+        img: 'Images/outnumbered.jpg',
+        name: 'Outnumbered',
+        artist: 'Dermot Kennedy',
+        music: 'Music/Dermot Kennedy -Outnumbered.mp3'
+    },
+    {
         img: 'Images/Arrdee come and go.jpg',
         name: 'Come Go',
         artist: 'Arrdee',
@@ -47,4 +53,91 @@ const music_list = [
         artist: 'Central Cee',
         music: 'Music/Central Cee  Obsessed With You Official Video.mp3'
     }
-]
+];
+
+loadTrack(track_index);
+
+function loadTrack(track_index) {
+    clearInterval(updateTimer);
+    reset();
+
+    curr_track.src = music_list[track_index].music;
+    curr_track.load();
+    track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+    track_name.textContent = music_list[track_index].name;
+    track_artist.textContent = music_list[track_index].artist;
+    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+
+    updateTimer = setInterval(setUpdate, 1000);
+
+    curr_track.addEventListener('ended', nextTrack);
+    random_bg_color();
+}
+function random_bg_color (){
+    let hex = ["0", "1", "2", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e"];
+    let a;
+
+    function populate (a) {
+        for (let i = 0; i < 6; i++) {
+            let x = Math.round(Math.random() * 14);
+            let y = hex[x]
+            a += y
+        }
+
+        return a
+    }
+
+    let color1 = populate('#');
+    let color2 = populate('#');
+    let angle = 'to right';
+
+    let gradient = 'linear-gradient(' + angle + ',' + Color1 + ', ' + Color2 + ")";
+    document.body.style.background = gradient
+}
+
+function reset() {
+    curr_time.textContent = "00:00";
+    total_duration.textContent = "00:00";
+    seek_slider.value = 0
+}
+
+function randomTrack  () {
+    isRandom ? pauseRandom() : playRandom();
+}
+
+function playRandom () {
+    isRandom = true;
+    randomIcon.classList.add('randomActive')
+}
+
+function pauseRandom  ()  {
+    isRandom = false;
+    randomIcon.classList.remove('randomActive')
+}
+
+function repeatTrack () {
+    let current_index = track_index;
+    loadTrack = current_index;
+    playTrack();
+};
+
+function playpauseTrack  () {
+    isPlaying ? pauseTrack() : playTrack();
+};
+
+function playTrack  () {
+    curr_track.play();
+    isPlaying = true;
+    track_art.classList.add('rotate');
+    wave.classList.add('loader');
+    playPause_btn.innerHTML = '<ion-icon name="pause-circle"></ion-icon>'
+};
+
+function pauseTrack () {
+    curr_track.pause();
+    isPlaying = false;
+    track_art.classList.remove('rotate');
+    wave.classList.remove('loader');
+    playPause_btn.innerHTML = '<ion-icon name="play-circle"></ion-icon>'
+}
+
